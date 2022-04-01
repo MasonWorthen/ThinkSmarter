884fced025d310f84f6bd20c86825d991bc90e0f
Mason Worthen <Mason.Worthen@msj.edu>
2022-04-01 14:20:35 -0400

    TS-9 Chart JS UI R

--- controllers/render.controller.js
+++ controllers/render.controller.js
@@ -37,9 +37,68 @@ function renderResults(req,res){
 function renderSurvey(req,res){
   res.render('survey',{'SurveyPageMessage':'','SurveyErrorMessage': ''});
 }
-function renderResearch(req,res){{
-    res.render("research",{});
-}}
+function renderResearch(req,res){
+    const db = firebase.getDatabase(firebase.firebaseApp)
+    const referenceCount = firebase.ref(db);
+
+    firebase.get(firebase.child(referenceCount, '/surveys')).then((snapshot) => {
+        if (snapshot.exists()) {
+            let data = snapshot.val();
+            console.log(data);
+            let keys = Object.keys(data);
+            let criTotal = 0;
+            let male = 0;
+            let female=0;
+            let non_binary = 0;
+            let memTotal = 0;
+            let decTotal = 0;
+
+            for(let x=0; x< keys.length;x++){
+                if(data[keys[x]]['improvementmem'] == 'Yes'){
+                    memTotal +=1;
+                }
+                if(data[keys[x]]['critical']= 'Yes'){
+                    criTotal +=1;
+                }
+                if(data[keys[x]]['improvementdec']='Yes'){
+                    decTotal +=1;
+                }
+
+                if(data[keys[x]]['gender'] == 'Male'){
+                    male +=1;
+                }
+                else if(data[keys[x]]['gender'] == 'Female'){
+                    female +=1;
+                }
+                else{
+                    non_binary +=1;
+                }    
+            }
+            const researchObj = {
+                'memory':memTotal,
+                'critical':criTotal,
+                'decision':decTotal,
+                'male':male,
+                'percentage':{
+                    'male':parseFloat((male/keys.length)*100).toFixed(2),
+                    'female':parseFloat((female/keys.length)*100).toFixed(2),
+                    'non-binary':parseFloat((non_binary/keys.length)*100).toFixed(2)
+                },
+                'female':female,
+                'non-binary':non_binary,
+                'total':keys.length
+            }
+            res.render("research",{"data":researchObj});
+            res.end();
+        } else {
+            res.render("research",{"data":{}});
+            res.end();
+        }
+    }).catch((error) => {
+        res.end(error)
+    });
+    }
+    
 function renderRegister(req,res){
     res.render('register',{'RegisterErrorMessage':''});
 }
---
+++ public/js/chart.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+ 
+    function renderPieChart(data,labels,label,element,container){
+        $(`#${element}`).remove();
+        $(container).append($(`<div class="col m-3" style="height:300px; width:300px;"> <canvas id="${element}"></canvas></div>`))
+        const ctx = $(`#${element}`);
+       
+        const myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels:labels,
+            datasets: [{
+              data: data,
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+              ],
+              hoverOffset: 4
+            }]
+        }    
+    });
+}
+ 
+    function renderBarChart(data,labels,label,element,container){
+        $(`#${element}`).remove();
+        $(container).append($(`<div class="col m-3" style="height:300px; width:300px;"> <canvas id="${element}"></canvas></div>`))
+        const ctx = $(`#${element}`);
+       
+        const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels:labels,
+            datasets: [{
+              data: data,
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+              ],
+              hoverOffset: 4
+            }]
+        }    
+    });
+}
+ 
+ 
+    renderPieChart([$('.male').text(),$('.female').text(),$('.other').text()], ['Male','Female','Other'],"Gender Group Improvement","myChart",".graphs");
+    renderPieChart([20,50,20], ['Male','Female','Other'],"Gender Group Improvement","myChart1",".graphs");
+    renderBarChart([$('.critical').text(),$('.memory').text(),$('.decide').text()], ['Memorization','CIT','DMS'],"Gender Group Improvement","myChart3",".graphs");
+ 
+ 
+  });
+
